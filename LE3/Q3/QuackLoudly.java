d18fbf9ba0a7d3951c3a8f44e70e53e1776e77e4
221B034 <221B034@juet.co.in>
2025-02-24 15:34:09 +0530

    Le

---
+++ LE3/Q3/QuackLoudly.java
@@ -0,0 +1,5 @@
+class QuackLoudly implements QuackBehavior {
+    public void quack() {
+        System.out.println("Quack! Quack!");
+    }
+}
---
+++ LE3/Q3/Swim.java
@@ -0,0 +1,5 @@
+class Swim implements SwimBehavior {
+    public void swim() {
+        System.out.println("I can swim!");
+    }
+}
